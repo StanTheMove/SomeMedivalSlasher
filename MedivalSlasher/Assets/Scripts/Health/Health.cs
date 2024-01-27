3a38fe28ebf0dcd9d95e2fe56eb1ddda01042e74
Stas <stas.kholopkin@gmail.com>
2024-01-27 19:09:18 +0200

    Added Health script!

--- MedivalSlasher/Assets/Scripts/Health/Health.cs
+++ MedivalSlasher/Assets/Scripts/Health/Health.cs
@@ -1,17 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] float currentHealth;
-    [SerializeField] float maxHealth;
-}
-public class PlayerHealth : Health
-{
+    [SerializeField] public float currentHealth;
+    [SerializeField] public float maxHealth = 100;
 
-}
-public class EnemyHealth : Health
-{
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
 
+    public void TakeDamage(float damageDealt)
+    {
+        currentHealth -= damageDealt;
+
+        if(currentHealth < damageDealt || currentHealth <= 0)
+        {
+            Object.Destroy(gameObject);
+        }
+    }
+    
+    public void Heal(float heal)
+    {
+        currentHealth += heal;
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    private void TestHealth()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            TakeDamage(20);
+        }
+    }
 }
