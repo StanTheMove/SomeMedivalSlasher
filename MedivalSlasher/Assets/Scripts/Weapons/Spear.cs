30c1f975c4f6a195ababf7390d38073fc7d5e4ba
RoMan_ <romangrygoryshyn@gmail.com>
2024-02-09 22:01:19 +0200

    fix bug

--- MedivalSlasher/Assets/Scripts/Weapons/Spear.cs
+++ MedivalSlasher/Assets/Scripts/Weapons/Spear.cs
@@ -1,25 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
-public class Sword : MonoBehaviour
+public class Spear : MonoBehaviour
 {
     public Transform Enemy;
-    public GameObject Sword_DH;
-    public int attackDamage = 5;
-    public float attackRange = 3f;
+    public GameObject spear;
+    public int attackDamage = 7;
+    public float attackRange = 10f;
     public LayerMask enemyLayer;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            StartCoroutine(SwordSwing());
-            SwordAttack();
+            StartCoroutine(SpearSwing());
+            SpearAttack();
         }
     }
-    void SwordAttack()
+    void SpearAttack()
     {
         Collider[] hitEnemy = Physics.OverlapSphere(transform.position, attackRange, enemyLayer);
         foreach (Collider enemy in hitEnemy)
@@ -32,10 +31,10 @@ public class Sword : MonoBehaviour
         }
     }
 
-    IEnumerator SwordSwing()
-    { 
-    Sword_DH.GetComponent<Animator>().Play("SwordSwing");
-    yield return new WaitForSeconds(1.0f);
-    Sword_DH.GetComponent<Animator>().Play("New State");
+    IEnumerator SpearSwing()
+    {
+        spear.GetComponent<Animator>().Play("New Animation");
+        yield return new WaitForSeconds(1.0f);
+        spear.GetComponent<Animator>().Play("New State");
     }
 }
